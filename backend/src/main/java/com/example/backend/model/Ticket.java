c65c361d6c685681d535b0f853d439fff076e422
s21009 <s21009@pjwstk.edu.pl>
2023-05-19 16:18:41 +0200

    git commit -m "add Ticket model"

--- backend/src/main/java/com/example/backend/model/Ticket.java
+++ backend/src/main/java/com/example/backend/model/Ticket.java
@@ -1,4 +1,37 @@
 package com.example.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TicketType ticketType;
+
+    private double price;
+
+    @PreUpdate
+    @PrePersist
+    private void updatePrice() {
+        switch (ticketType) {
+            case ADULT -> price = 25.0;
+            case CHILD -> price = 18.0;
+            case STUDENT -> price = 12.50;
+            default -> throw new IllegalArgumentException("Invalid ticket type");
+        }
+
+    }
+}
+
+enum TicketType {
+    ADULT, CHILD, STUDENT
 }
